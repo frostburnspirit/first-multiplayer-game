2f3407f2c93bd82047b8f0e8287560ea7f465b39
frostburnspirit <frostburnspirit@gmail.com>
2024-03-18 12:46:13 +0100

    Keypress detection

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -15,9 +15,34 @@ function updatePlayer() {
     };
   } else {
     drawObjects.layer1.player.x += 1;
-    drawObjects.layer1.player.radius += 1;
   }
 }
+
+// c.canvas.addeven
+let keysPressed = {
+  up: undefined,
+  down: undefined,
+  left: undefined,
+  right: undefined,
+};
+document.addEventListener("keydown", (e) => {
+  // Maybe check for keys and key combinations that aren't relevant too? https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
+
+  // Which one to choose?
+
+  if (
+    (e.code === "ArrowUp" || e.code === "KeyW") &&
+    !Object.values(keysPressed).includes("up")
+  ) {
+    console.log(e.code);
+  }
+
+  // if ((e.code === "ArrowUp" || e.code === "KeyW") && !keysPressed.up) {
+  //   console.log(e.code);
+  // }
+});
+
+// addeventlistener keyup
 
 function main() {
   draw.clearCanvas(c.canvas, c.ctx);
@@ -25,6 +50,8 @@ function main() {
   draw.draw(c.ctx, drawObjects); // objects is an objects containing different drawing layers, each cointaining an object with objects {layer1: {obj1:obj1, obj2:something, obj3:test}, layer2: {obj4:obj4, obj5:placeholder} ...}
   draw.nextFrame(main, c.fps);
 }
+
+// executing scripts
 
 ini.ini(c.canvas, c.resolution);
 
